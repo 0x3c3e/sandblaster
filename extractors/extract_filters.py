14ba26bec09bb7e3ca231b819c121418be230007
Arsenii Kostromin <kostromin.arsenii@ya.ru>
2025-01-11 20:30:34 +0300

    Parse filters correctly

--- extractors/extract_filters.py
+++ extractors/extract_filters.py
@@ -24,6 +24,18 @@ def get_cstring(target_macho, offset):
     return value
 
 
+def extract_modifiers(target_macho, start_address):
+    output = {}
+    while True:
+        offset = target_macho.get_int_from_virtual_address(start_address, 4)
+        if offset == 0:
+            break
+        name = get_cstring(target_macho, offset)
+        func = target_macho.get_int_from_virtual_address(start_address + 0x8, 1)
+        start_address += 0x10
+        output[func] = name
+    return output
+
 def extract_data_between_variables(file_path, output_path):
     output = {}
     fat_binary = lief.MachO.parse(file_path)
@@ -31,12 +43,16 @@ def extract_data_between_variables(file_path, output_path):
     start_address = target_macho.get_symbol("_filter_info").value + 0x20
     end_address = target_macho.get_symbol("_modifier_info").value - 0x20
     for i in range(int((end_address - start_address) / 0x20)):
+        mods = None
         offset = target_macho.get_int_from_virtual_address(start_address, 4)
         key = i + 1
         name = get_cstring(target_macho, offset)
         func = target_macho.get_int_from_virtual_address(start_address + 0x10, 1)
+        modifiers = target_macho.get_int_from_virtual_address(start_address + 0x18, 4)
+        if modifiers:
+            mods = extract_modifiers(target_macho, modifiers + 0x180000000)
         start_address += 0x20
-        output[hex(key)] = {"name": name, "arg_process_fn": mapping[func]}
+        output[hex(key)] = {"name": name, "arg_process_fn": mapping[func], "modifiers": mods}
     length = len(output) - 1
     for key in range(1, length):
         new_key = hex(key + length + 0x20)
@@ -45,7 +61,7 @@ def extract_data_between_variables(file_path, output_path):
             "arg_process_fn": "get_filter_arg_regex_by_id" if "string" in output[hex(key)]["arg_process_fn"] else "get_filter_arg_octal_integer",
         }
     with open(output_path, "w") as file:
-        json.dump(output, file, indent=4)
+        json.dump(output, file, indent=4, sort_keys=int)
 
 
 if __name__ == "__main__":
