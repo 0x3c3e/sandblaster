ef8ef1b0a5103202e5a732d4b991d51553292a94
Arsenii Kostromin <kostromin.arsenii@ya.ru>
2025-01-11 15:46:16 +0300

    Update filters

---
+++ extractors/extract_filters.py
@@ -0,0 +1,61 @@
+import sys
+import lief
+import json
+
+mapping = {
+    1: "get_filter_arg_boolean",
+    2: "get_filter_arg_octal_integer",
+    3: "get_filter_arg_socket_domain",
+    4: "get_filter_arg_string_by_offset_no_skip",
+    5: "get_filter_arg_string_by_offset",
+    6: "get_filter_arg_string_by_offset",
+    7: "get_filter_arg_string_by_offset_with_type",
+    8: "get_filter_arg_network_address",
+}
+
+
+def get_cstring(target_macho, offset):
+    value = ""
+    while char := target_macho.get_content_from_virtual_address(
+        0x180000000 + offset, 1
+    )[0]:
+        value += chr(char)
+        offset += 1
+    return value
+
+
+def extract_data_between_variables(file_path, output_path):
+    output = {}
+    fat_binary = lief.MachO.parse(file_path)
+    target_macho = fat_binary.take(lief.MachO.Header.CPU_TYPE.ARM64)
+    start_address = target_macho.get_symbol("_filter_info").value + 0x20
+    end_address = target_macho.get_symbol("_modifier_info").value - 0x20
+    for i in range(int((end_address - start_address) / 0x20)):
+        offset = target_macho.get_int_from_virtual_address(start_address, 4)
+        key = i + 1
+        name = get_cstring(target_macho, offset)
+        func = target_macho.get_int_from_virtual_address(start_address + 0x10, 1)
+        start_address += 0x20
+        output[hex(key)] = {"name": name, "arg_process_fn": mapping[func]}
+    length = len(output) - 1
+    for key in range(1, length):
+        new_key = hex(key + length + 0x20)
+        if "string" not in output[hex(key)]["arg_process_fn"]:
+            continue
+        output[new_key] = {
+            "name": output[hex(key)]["name"],
+            "arg_process_fn": "get_filter_arg_regex_by_id",
+        }
+    with open(output_path, "w") as file:
+        json.dump(output, file, indent=4)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(
+            "Usage: python extract_profile_data_from_kext.py <file_path> <output_path>"
+        )
+        exit(1)
+    file_path = sys.argv[1]
+    output_path = sys.argv[2]
+    extract_data_between_variables(file_path, output_path)
--- extractors/extract_sandbox_operations.py
+++ extractors/extract_sandbox_operations.py
@@ -1,39 +1,44 @@
 import sys
 import lief
 
+
 def extract_sandbox_operations(binary):
     extracted_strings = []
 
     for section in binary.sections:
         if section.type == lief.MachO.Section.TYPE.CSTRING_LITERALS:
             strings_bytes = section.content.tobytes()
-            strings = strings_bytes.decode('utf-8', errors='ignore')
-            extracted_strings.extend(strings.split('\x00'))
+            strings = strings_bytes.decode("utf-8", errors="ignore")
+            extracted_strings.extend(strings.split("\x00"))
 
     operations = []
     capture = False
 
     for string in extracted_strings:
-        if string == 'default':
+        if string == "default":
             capture = True
         if capture:
             operations.append(string)
-        if string == 'xpc-message-send':
+        if string == "xpc-message-send":
             capture = False
 
     return operations
+
 
 def main(input_file, output_file):
     binary = lief.parse(input_file)
     operations = extract_sandbox_operations(binary)
 
-    with open(output_file, 'w') as f:
+    with open(output_file, "w") as f:
         for operation in operations:
-            f.write(operation + '\n')
+            f.write(operation + "\n")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python extract_sandbox_operations.py <path_to_macho_binary> <output_file>")
+        print(
+            "Usage: python extract_sandbox_operations.py <path_to_macho_binary> <output_file>"
+        )
         sys.exit(1)
 
     input_file = sys.argv[1]
