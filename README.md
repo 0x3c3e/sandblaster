bbd23e0d4c653fe0c1b27be1976c41693bf3f074
Arsenii Kostromin <kostromin.arsenii@ya.ru>
2025-06-25 19:26:37 +0300

    Update project

--- README.md
+++ README.md
@@ -17,29 +17,29 @@ python extractors/extract_profile_data_from_kext.py Sandbox.kext/Contents/MacOS/
 ```
 
 ## Testing
-
-Create a virtual environment and install the required packages:
-
-```sh
-pip install -r requirements.txt
-```
-
 Run the test suite with `pytest`:
 
 ```sh
 pytest -q
 ```
 
+## Installation
+
+```sh
+python -m venv venv
+source venv/bin/activate
+pip install -e .
+```
+
 ### Reversing the Sandbox
 After extracting the necessary data, run the following command to reverse the sandbox profile:
 
 ```sh
-python -m sandblaster --operations profiles/sandbox_operations profiles/profile_data --output profiles/profile_data_reversed
+sandblaster --operations profiles/sandbox_operations profiles/profile_data --output profiles/profile_data_reversed
 ```
 
 ## Credits
 
-- [SandBlaster by Cellebrite Labs by Yarden Hamami](https://github.com/cellebrite-labs/sandblaster)
 - [Malus Security SandBlaster Repository](https://github.com/malus-security/sandblaster)
 - [XNUSandbox by Dionysus Blazakis](https://github.com/dionthegod/XNUSandbox)
 - [Sandbox Toolkit by Stefan Esser](https://github.com/sektioneins/sandbox_toolkit)
---
+++ sandblaster/cli/main.py
@@ -0,0 +1,58 @@
+import argparse
+from sandblaster.parsers.header import SandboxHeader
+from sandblaster.parsers.profile import SandboxPayload
+from sandblaster.parsers.graph import GraphParser
+from sandblaster.configs.filters import Filters
+from importlib.resources import files
+
+
+def process_profile(output_path: str, payload: SandboxPayload) -> None:
+    """
+    Build and output the operation node graphs for selected operations.
+    """
+    with open(output_path, "wt") as outfile:
+        outfile.write("(version 1)\n")
+
+        for idx, offset in enumerate(payload.op_table):
+            operation = payload.sb_ops[idx]
+
+            if payload.ops_to_reverse and operation not in payload.ops_to_reverse:
+                continue
+
+            node = payload.operation_nodes.find_operation_node_by_offset(offset)
+            if not node:
+                continue
+
+            graph_builder = GraphParser(node)
+            graph = graph_builder.build_operation_node_graph()
+
+            # graph_builder.export_dot("../dots/out.dot")
+            for graph_node_offset in graph.nodes:
+                graph_node = payload.operation_nodes.find_operation_node_by_offset(
+                    graph_node_offset
+                )
+                if graph_node:
+                    print(graph_node_offset, graph_node, graph_node.raw)
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(description="Apple Sandbox Profiles Decompiler")
+    parser.add_argument("filename")
+    parser.add_argument(
+        "--operations",
+        required=True,
+    )
+    parser.add_argument("--filter")
+    parser.add_argument("--output", required=True)
+
+    args = parser.parse_args()
+
+    filters = Filters(files("sandblaster.misc") / "filters.json")
+    modifiers = Filters(files("sandblaster.misc") / "modifiers.json")
+    with open(args.filename, "rb") as infile:
+        sandbox_data = SandboxHeader.from_file(infile)
+        payload = SandboxPayload(infile=infile, base_addr=sandbox_data.base_addr)
+        payload.parse(sandbox_data, args.operations, args.filter, filters, modifiers)
+
+    process_profile(args.output, payload)
+    return 0
