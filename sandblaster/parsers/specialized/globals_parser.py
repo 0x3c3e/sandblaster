c54aa63fd95dd84e0f3b85723e7c54cdd2d177e7
Arsenii Kostromin <kostromin.arsenii@ya.ru>
2025-06-25 14:27:48 +0300

    Fix globals

--- sandblaster/parsers/specialized/globals_parser.py
+++ sandblaster/parsers/specialized/globals_parser.py
@@ -5,12 +5,12 @@ from typing import List, BinaryIO
 class GlobalVarsParser:
     @staticmethod
     def parse(infile: BinaryIO, base_addr: int, count: int, offset: int) -> List[str]:
-        global_vars = set()
+        global_vars = []
         for i in range(count):
             infile.seek(offset + i * 2)
             var_offset = struct.unpack("<H", infile.read(2))[0]
             infile.seek(base_addr + var_offset * 8)
             strlen = struct.unpack("<H", infile.read(2))[0]
             string = infile.read(strlen - 1).decode("utf-8")
-            global_vars.add(string)
+            global_vars.append(string)
         return global_vars
